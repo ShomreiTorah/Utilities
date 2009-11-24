14cc4a583363fbade8c25691cfc8c7f506276410
Schabse Laks <Schabse@GMail.com>
2009-11-24 09:30:18 -0500

    Upload update
    [git-p4: depot-paths = "//depot/Shul/Utilities/": change = 1447]

--- ShomreiTorah.UpdatePublisher/Program.cs
+++ ShomreiTorah.UpdatePublisher/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Windows.Forms;
 using System.IO.Compression;
 using System.Security.Cryptography;
+using ShomreiTorah.Common;
+using ShomreiTorah.Common.Updates;
+using System.Net;
+using ShomreiTorah.WinForms.Forms;
+using DevExpress.XtraEditors;
 
 namespace ShomreiTorah.UpdatePublisher {
 	static class Program {
@@ -29,15 +34,38 @@ namespace ShomreiTorah.UpdatePublisher {
 
 			var element = EntryForm.Show(baseDir);
 			if (element == null) return;
-			//var encryptor = new RijndaelManaged { BlockSize = 256, KeySize = 256 };
-			//byte[] hash;
+			if (ProgressWorker.Execute(ui => {
+				var encryptor = new RijndaelManaged { BlockSize = 256, KeySize = 256 };
+				byte[] hash;
 
-			//using (var transform = encryptor.CreateDecryptor())
-			//    using (var hasher = new SHA512CryptoServiceProvider())
-			//    using (var unzipper = new GZipStream(hashingStream, CompressionMode.Decompress)) {
-			//    using (var decryptingStream = new CryptoStream(cypherStream, transform, CryptoStreamMode.Read))
-			//    using (var hashingStream = new CryptoStream(decryptingStream, hasher, CryptoStreamMode.Read))
-			//    }
+				var fullUri = new Uri(UpdateChecker.BaseUri, new Uri(element.Attribute("Name").Value + ".Update", UriKind.Relative));
+				var ftpRequest = FtpClient.Default.CreateRequest(new Uri(UpdateChecker.BaseUri.GetLeftPart(UriPartial.Authority), UriKind.Absolute).MakeRelativeUri(fullUri));
+				ftpRequest.Method = WebRequestMethods.Ftp.UploadFile;
+				if (ui.WasCanceled) return;
+
+				ui.Caption = "Connecting to server...";
+				using (var transform = encryptor.CreateEncryptor())
+				using (var hasher = new SHA512CryptoServiceProvider())
+
+				using (var requestStream = ftpRequest.GetRequestStream())
+				using (var zipper = new GZipStream(requestStream, CompressionMode.Compress))
+				using (var hashingStream = new CryptoStream(zipper, hasher, CryptoStreamMode.Write))
+				using (var encryptingStream = new CryptoStream(hashingStream, transform, CryptoStreamMode.Write)) {
+					UpdateStreamer.WriteArchive(encryptingStream, baseDir, ui);
+					encryptingStream.FlushFinalBlock();
+
+					hash = hasher.Hash;
+				}
+				if (ui.WasCanceled)
+					return;
+				ui.Caption = "Uploading update...";
+				ui.Maximum = -1;
+				ftpRequest.GetResponse().Close();
+
+				//TODO: Blob
+			}, true))
+				XtraMessageBox.Show("The update has been uploaded to the server.",
+									"Shomrei Torah Update Publisher", MessageBoxButtons.OK, MessageBoxIcon.Information);
 		}
 	}
 }
