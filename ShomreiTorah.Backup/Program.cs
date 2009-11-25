86b488743ce966fb32599214860c3c3771f40966
Schabse Laks <Schabse@GMail.com>
2009-11-25 18:56:10 -0500

    Horrible idea
    [git-p4: depot-paths = "//depot/Shul/Utilities/": change = 1491]

--- ShomreiTorah.Backup/Program.cs
+++ ShomreiTorah.Backup/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using ShomreiTorah.Common;
 using System.IO;
+using System.Threading;
 
 namespace ShomreiTorah.Backup {
 	class Program {
@@ -13,6 +14,7 @@ namespace ShomreiTorah.Backup {
 			} catch (Exception ex) {
 				Email.Warn("Database Backup Exception", ex.ToString());
 			}
+			Thread.Sleep(TimeSpan.FromSeconds(10));	//Give the async error emails time to finish.  Yes, I know that this is a horrible thing to do.
 		}
 		static void ExecOperation(IEnumerator<string> operation, string name) {
 			using (operation) {
@@ -22,7 +24,7 @@ namespace ShomreiTorah.Backup {
 						if (!operation.MoveNext()) return;
 						current = operation.Current;
 					} catch (Exception ex) {
-						Email.Warn(name + "Backup Exception" + current, ex.ToString());
+						Email.Warn(name + " Backup Exception: " + current, ex.ToString());
 					}
 				}
 			}
