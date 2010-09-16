2b742a47aeb010130ae56dc592239697f275cb65
Schabse Laks <Schabse@GMail.com>
2010-09-16 13:05:58 -0500

    Suppress warnings
    [git-p4: depot-paths = "//depot/Shul/Utilities/": change = 2447]

--- ShomreiTorah.Singularity.Designer/Model Classes.cs
+++ ShomreiTorah.Singularity.Designer/Model Classes.cs
@@ -1,23 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.Linq;
 using System.Text;
-using System.ComponentModel;
 using System.Windows.Forms;
-using System.Globalization;
 using ShomreiTorah.Singularity.Designer.Model;
 
 namespace ShomreiTorah.Singularity.Designer {
 	class ModelBindingSource : BindingSource {
 		public ModelBindingSource() : base(new BindingList<SchemaModel>(), null) { }
 
+		[SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "Override for designer properties")]
 		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
 		[Browsable(false)]
 		public new object DataSource {
 			get { return base.DataSource; }
 			set { base.DataSource = value; }
 		}
+		[SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "Override for designer properties")]
 		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
 		[Browsable(false)]
 		public new string DataMember {
