e1328c591449e7f5b88c92bc55eb652cf7634319
Schabse Laks <Schabse@GMail.com>
2011-01-16 12:55:43 -0500

    Prepare for new journal
    [git-p4: depot-paths = "//depot/Shul/Utilities/": change = 3276]

--- ShomreiTorah.Backup/JournalBackup.cs
+++ ShomreiTorah.Backup/JournalBackup.cs
@@ -7,7 +7,42 @@ using ShomreiTorah.Common;
 using System.Globalization;
 
 namespace ShomreiTorah.Backup {
-	static class JournalBackup {
+	static class SimpleJournalBackup {
+		//static readonly string[] paths = { @"L:\Community\Rabbi Weinberger's Shul\Melave Malka\{0}\Journal\Journal.pptx" };
+
+		//public static IEnumerator<string> DoBackup() {
+		//    var journalPath = Config.ReadAttribute("Journal", "Path");
+
+		//    var backupFolder = Environment.ExpandEnvironmentVariables(
+		//        String.Format(CultureInfo.InvariantCulture, Config.ReadAttribute("Journal", "BackupPath"), DateTime.Now)
+		//    );
+
+		//    var year = DateTime.Now.AddMonths(6).Year;
+		//    var files = paths.Select(f=>str).Select(file => new {
+		//        Name = file,
+		//        SourcePath = Path.Combine(journalPath, file),
+		//        DestPath = Path.Combine(backupFolder, file)
+		//    });
+
+		//    if (files.Any(f => !File.Exists(f.SourcePath)))
+		//        yield break;
+
+		//    Directory.CreateDirectory(Path.GetDirectoryName(backupFolder));
+
+		//    if (files.Any(f =>	//If any of the files don't have existing equal copies,
+		//        !Directory.GetFiles(Path.GetDirectoryName(backupFolder), f.Name, SearchOption.AllDirectories).Any(p => Program.AreEqual(f.SourcePath, p))
+		//    )) {
+		//        Directory.CreateDirectory(backupFolder);
+		//        foreach (var file in files) {
+		//            yield return file.Name;
+
+		//            File.Copy(file.SourcePath, file.DestPath);
+		//        }
+		//    }
+		//}
+	}
+
+	static class LegacyJournalBackup {
 
 		static string[] fileNames = new[] { "Journal.pptx", "Ads.xml" };
 
--- ShomreiTorah.Backup/Program.cs
+++ ShomreiTorah.Backup/Program.cs
@@ -10,7 +10,7 @@ namespace ShomreiTorah.Backup {
 	class Program {
 		static void Main() {
 			try {
-				ExecOperation(JournalBackup.DoBackup(), "Journal");
+				ExecOperation(LegacyJournalBackup.DoBackup(), "Journal");
 				ExecOperation(DbBackup.DoBackup(), "Database");
 			} catch (Exception ex) {
 				Email.Warn("Backup Exception", ex.ToString());
